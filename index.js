791f6b63fc3722f7247fd1581cd2e39a606ef660
1onlinesolution <team@damianos.io>
2021-06-07 09:08:30 +0100

    exporting ObjectID from mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+exports.ObjectId = require('mongodb').ObjectID;
 exports.MongoConnection = require('./lib/mongoConnection');
 exports.MongoDatabase = require('./lib/mongoDatabase');
 exports.MongoCollection = require('./lib/mongoCollection');
